83d6da99a3196ede1f0473bf2ab73a1687af7839
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-15 21:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function removeColor() {
+    const colorSelect = document.getElementById("colorSelect");
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    colorSelect.removeChild(selectedOption);
+  }
